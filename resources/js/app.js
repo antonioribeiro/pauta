6e583af97a8540b6100aa139e65336aa398adcff
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2019-04-04 12:18:05 -0300

    Link generator

--- resources/js/app.js
+++ resources/js/app.js
@@ -29,5 +29,37 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  */
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+
+    data: {
+        notesLink: null,
+        portalId: null,
+    },
+
+    computed: {
+        siteLink() {
+            const link = new RegExp('.*alerjln1.alerj.rj.gov.br(.*)').exec(this.notesLink);
+
+            if (link && link[1]) {
+                return "http://www3.alerj.rj.gov.br/lotus_notes/default.asp?id="+this.portalId+"&url=" +
+                    btoa(link[1] + '?OpenDocument&ExpandView')
+            }
+
+            return ''
+        }
+    },
+
+    mounted() {
+        new ClipboardJS('.copy');
+    }
 });
+
+
+// http://alerjln1.alerj.rj.gov.br/scpro1923.nsf/f4b46b3cdbba990083256cc900746cf6/B8E8D0BD4D58DAE2832583C900613C46
+
+//
+// /scpro1923.nsf/f4b46b3cdbba990083256cc900746cf6/B8E8D0BD4D58DAE2832583C900613C46
+// http://www3.alerj.rj.gov.br/lotus_notes/default.asp?id=144&url=L3NjcHJvMTkyMy5uc2YvZjRiNDZiM2NkYmJhOTkwMDgzMjU2Y2M5MDA3NDZjZjYvQjhFOEQwQkQ0RDU4REFFMjgzMjU4M0M5MDA2MTNDNDY/T3BlbkRvY3VtZW50JkV4cGFuZFZpZXc=
+
+
+
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -10,12 +10,25 @@
                     <div class="card-body">
                         <form>
                             <div class="form-group">
-                                <label for="exampleInputEmail1">Link Notes</label>
-                                <input class="form-control" placeholder="">
+                                <label for="exampleInputEmail1">ID do Portal</label>
+                                <input v-model="portalId" class="form-control" placeholder="">
                             </div>
+
                             <div class="form-group">
-                                <label for="exampleInputPassword1">Link Portal</label>
-                                <input class="form-control" placeholder="">
+                                <label for="exampleInputEmail1">Link Notes</label>
+                                <input v-model="notesLink" class="form-control" placeholder="">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="exampleInputPassword1">
+                                    Link Portal
+
+                                    <a href="#" class="copy" data-clipboard-target="#siteLink">
+                                        (copiar)
+                                    </a>
+                                </label>
+
+                                <input v-model="siteLink" id="siteLink" class="form-control" placeholder="">
                             </div>
                         </form>
                     </div>
